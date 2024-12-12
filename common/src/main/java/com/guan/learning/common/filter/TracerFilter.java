39c52cea84805434892ab0fff186207ea4667b72
guan <657836361@qq.com>
2024-12-12 23:04:16 +0800

    增加springboot3版本bean自动导入逻辑

--- common/src/main/java/com/guan/learning/common/filter/TracerFilter.java
+++ common/src/main/java/com/guan/learning/common/filter/TracerFilter.java
@@ -10,11 +10,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.MDC;
 import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @WebFilter
+@Component
 @Order(1)
 public class TracerFilter extends OncePerRequestFilter {
     @Override
