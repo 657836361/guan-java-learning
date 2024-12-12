1475684d8e5dc4fe2758bbf2d8a2c4bac121f27b
guan <657836361@qq.com>
2024-12-12 20:48:47 +0800

    删除spring-lifecycle 把代码挪到spring-test中

---
+++ common/src/main/java/com/guan/learning/common/bean/MybatisPlusConfig.java
@@ -0,0 +1,35 @@
+package com.guan.learning.common.bean;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 使用多个插件时，需要注意它们的顺序。建议的顺序是：
+ * 多租户、动态表名
+ * 分页、乐观锁
+ * SQL 性能规范、防止全表更新与删除
+ * 总结：对 SQL 进行单次改造的插件应优先放入，不对 SQL 进行改造的插件最后放入。
+ */
+@Configuration
+@ConditionalOnBean(MybatisPlusAutoConfiguration.class)
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     *
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor paginationInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 如果配置多个插件, 切记分页最后添加
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
