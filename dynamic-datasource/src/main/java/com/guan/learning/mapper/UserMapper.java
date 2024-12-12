5d6a7e0b46558a00ff2bf15a6061c71b668a03e7
guan <657836361@qq.com>
2024-12-12 20:31:13 +0800

    删除dynamic-datasource module

--- dynamic-datasource/src/main/java/com/guan/learning/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.guan.learning.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.guan.learning.pojo.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
