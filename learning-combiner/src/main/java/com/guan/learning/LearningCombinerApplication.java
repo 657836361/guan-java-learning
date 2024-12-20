cb89b4984e644ea5aae3560108f80dea1567a9df
guanp <guanp@bsfit.com.cn>
2024-12-20 08:40:38 +0800

    learning-combiner 启动类过滤器的正则写法

--- learning-combiner/src/main/java/com/guan/learning/LearningCombinerApplication.java
+++ learning-combiner/src/main/java/com/guan/learning/LearningCombinerApplication.java
@@ -1,7 +1,6 @@
 package com.guan.learning;
 
 import com.guan.learning.anno.EnableDataSource;
-import com.guan.learning.spring.lifecycle.SpringLifeTypeFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -13,7 +12,9 @@ import org.springframework.context.annotation.FilterType;
  */
 //@EnableDataSource(mode = DataSourceMode.NORMAL)
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.CUSTOM, classes = SpringLifeTypeFilter.class))
+// 两种都行
+//@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.CUSTOM, classes = SpringLifeTypeFilter.class))
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com.guan.learning.spring.lifecycle.*"))
 public class LearningCombinerApplication {
     public static void main(String[] args) {
         SpringApplication.run(LearningCombinerApplication.class, args);
