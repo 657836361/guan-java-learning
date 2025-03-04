3a33482c0c8aff11e3ab6c6ba09257c7595e2161
guanp <guanp@bsfit.com.cn>
2025-03-04 11:16:05 +0800

    优化redis配置

--- learning-combiner/src/main/java/com/guan/learning/redis/controller/RedisController.java
+++ learning-combiner/src/main/java/com/guan/learning/redis/controller/RedisController.java
@@ -1,5 +1,6 @@
 package com.guan.learning.redis.controller;
 
+import cn.hutool.core.util.IdUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +13,22 @@ public class RedisController {
 
     @Autowired(required = false)
     private StringRedisTemplate redisTemplate;
-    
+
 
     @GetMapping("/test")
-    public String redisTest(){
+    public String redisTest() {
         if (redisTemplate != null) {
             return "redis inited";
         }
         return "redis fail to init";
+    }
+
+    @GetMapping("")
+    public String add() {
+        for (int i = 0; i < 10000; i++) {
+            String uuid = IdUtil.fastUUID();
+            redisTemplate.opsForValue().set(uuid, uuid);
+        }
+        return "ok";
     }
 }
